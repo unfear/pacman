049b3d353accfd9bc9721d3b435f769805fcb3e4
Volodymyr Andrashko <vandrashko@luxoft.com>
2015-10-23 16:49:27 +0300

    Move work to home

--- pacman.py
+++ pacman.py
@@ -1,4 +1,6 @@
 import pyglet
+from pyglet.window import key
+
 win = pyglet.window.Window(width=496, height=290)
 pyglet.gl.glClearColor(0.3, 0.1, 0.5, 1)
 
@@ -8,7 +10,8 @@ label = pyglet.text.Label('Crash', font_name='Times New Roman', font_size=36, x=
 
 imgPacmanSprite = pyglet.sprite.Sprite(imgPacman)
 imgEnemySprite = pyglet.sprite.Sprite(imgEnemy)
-imgPacmanSprite.x = win.width/2
+imgPacmanSprite.x = (win.width - imgPacmanSprite.width) / 2
+imgPacmanSprite.y = (win.height - imgPacmanSprite.width) / 2
 
 # state of pacman direction
 pacmanDirection = 'right'
@@ -45,34 +48,35 @@ pyglet.clock.schedule_interval(update, 0.01)
 @win.event
 def on_key_press(symbol, modifiers):
     # ... handle this event ...
-    from pyglet.window import key
+
+    # if symbol == key.LEFT:
+    #     imgPacmanSprite.image = imgPacman.get_transform(False, True, 180)
+    # if symbol == key.RIGHT:
+    #     imgPacmanSprite.image = imgPacman.get_transform(False, False, 360)
+    # if symbol == key.UP:
+    #     imgPacmanSprite.image = imgPacman.get_transform(True, False, 90)
+    # if symbol == key.DOWN:
+    #     imgPacmanSprite.image = imgPacman.get_transform(False, False, 90)
     if symbol == key.LEFT:
-        imgPacmanSprite.rotation = 180
-        # imgPacmanSprite.x -= pacmanStep
-
+        imgPacmanSprite.image = imgPacman.get_transform(True, False, 0)
     if symbol == key.RIGHT:
-        imgPacmanSprite.rotation = 0
-        # imgPacmanSprite.x += pacmanStep
-
+        imgPacmanSprite.image = imgPacman.get_transform(False, False, 180)
     if symbol == key.UP:
-        imgPacmanSprite.rotation = 270
-        # imgPacmanSprite.y += pacmanStep
-
+        imgPacmanSprite.image = imgPacman.get_transform(True, False, 90)
     if symbol == key.DOWN:
-        imgPacmanSprite.rotation = 90
-        # imgPacmanSprite.y -= pacmanStep
+        imgPacmanSprite.image = imgPacman.get_transform(False, False, 90)
 
 
-# @win.event
-# def on_text_motion(motion):
-#     from pyglet.window import key
-#     if motion == key.MOTION_LEFT:
-#         imgPacmanSprite.x -= pacmanStep
-#     if motion == key.MOTION_RIGHT:
-#         imgPacmanSprite.x += pacmanStep
-#     if motion == key.MOTION_UP:
-#         imgPacmanSprite.y += pacmanStep
-#     if motion == key.MOTION_DOWN:
-#         imgPacmanSprite.y -= pacmanStep
+@win.event
+def on_text_motion(motion):
+    from pyglet.window import key
+    if motion == key.MOTION_LEFT:
+        imgPacmanSprite.x -= pacmanStep
+    if motion == key.MOTION_RIGHT:
+        imgPacmanSprite.x += pacmanStep
+    if motion == key.MOTION_UP:
+        imgPacmanSprite.y += pacmanStep
+    if motion == key.MOTION_DOWN:
+        imgPacmanSprite.y -= pacmanStep
 
 pyglet.app.run()
