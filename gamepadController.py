ac6d58e7fcf59b83b951e116f1217bb5b525b6b9
unfear <unfeararies@gmail.com>
2019-09-03 21:06:10 +0300

    Gamepad hat works.

--- gamepadController.py
+++ gamepadController.py
@@ -1,6 +1,7 @@
 import pyglet
 from pyglet.window import key
 
+
 class GamepadController:
     def __init__(self):
         self.active = False
@@ -10,34 +11,34 @@ class GamepadController:
             joystick = joysticks[0]
             joystick.open()
             joystick.push_handlers(self)
-            active = True
+            self.active = True
         else:
             print("There is not Gamepad connected!")
 
     def getPress(self):
         return self.direction
 
-    def on_joybutton_press(joystick, button):
-        pass
+    # def on_joybutton_press(self, joystick, button):
+    #     pass
+    #
+    # def on_joybutton_release(self, joystick, button):
+    #     pass
+    #
+    # def on_joyaxis_motion(self, joystick, axis, value):
+    #     pass
 
-    def on_joybutton_release(joystick, button):
-        pass
-
-    def on_joyaxis_motion(joystick, axis, value):
-        pass
-
-    def on_joyhat_motion(joystick, hat_x, hat_y):
+    def on_joyhat_motion(self, joystick, hat_x, hat_y):
+        print("Hat ({}, {})".format(hat_x, hat_y))
         if hat_x != 0:
             if hat_x == 1:
-                global direction
-                direction = key.RIGHT
+                self.direction = key.RIGHT
             if hat_x == -1:
-                direction = key.LEFT
+                self.direction = key.LEFT
 
         if hat_y != 0:
             if hat_y == 1:
-                direction = key.UP
+                self.direction = key.UP
             if hat_y == -1:
-                direction = key.DOWN
+                self.direction = key.DOWN
 
         pass
